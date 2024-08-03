58607c28de89ee64a944976ed05798d512306683
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-08-03 11:50:40 -0400

    part 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,40 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  
   console.log("puppyList: ", puppyList)
+  
+  function handleClick() {
+    // some logic here
+  }
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+
   return (
     <>
       <div className="App">
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
+        {puppies.map((puppy) => {
+            return onClick={()=>{ setFeatPupId(puppy.id)}} 
+            <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
           })
         }
+
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+
+        </div>
+      )}
+    
+      {console.log(featPupId);}
         {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
