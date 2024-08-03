bbd23a0cf2b66a4dbffb0cfdff1bb698e2bb00b3
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-08-03 11:11:39 -0400

    part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-/* import { useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg' */
+import viteLogo from '/vite.svg'
+
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  /* const [count, setCount] = useState(0) */
-
+  const [puppies, setPuppies] = useState(puppies)
+  console.log(puppyList)
   return (
     <>
       <div>
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
         {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
